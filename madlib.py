f70ba7bae009a8d549527ef434d5c6dcf9c4fcef
pc <prayag.9503@gmail.com>
2021-06-15 18:47:58 +0530

    progress

---
+++ madlib.py
@@ -0,0 +1,14 @@
+
+color = input("enter a color : ")
+plural_noun = input("enter a plural noun : ")
+wrestler = input("enter a wrestler : ")
+number = input("enter any number : ")
+
+
+
+print("days are " + color)
+print(plural_noun + " is patchy")
+print("the name is " + wrestler)
+print("but i wont give in")
+print("so i want a " + number )
+
